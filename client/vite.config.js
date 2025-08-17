0596fb872425156e1f8c1d9b79c361a4f9795379
Sahil khan <sk.sahilkhan543@gmail.com>
2025-08-17 17:21:01 +0530

    updated backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ai-meeting-notes-summarizer-phi.vercel.app/',
         changeOrigin: true,
         secure: false,
       }
